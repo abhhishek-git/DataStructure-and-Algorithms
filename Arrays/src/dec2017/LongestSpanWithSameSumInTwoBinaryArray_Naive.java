89e81da0b5f936f98555b6b4c6e239d6f69c1678
abhishek <abhiwanttochat@gmail.com>
2017-11-29 08:42:31 +0530

    both arrays will have same length. So replacing len1 and len2 with len

--- Arrays/src/dec2017/LongestSpanWithSameSumInTwoBinaryArray_Naive.java
+++ Arrays/src/dec2017/LongestSpanWithSameSumInTwoBinaryArray_Naive.java
@@ -4,30 +4,29 @@ public class LongestSpanWithSameSumInTwoBinaryArray_Naive {
 	public static void main(String[] args) {
 		int[] arr1 = {0,1,0,1,1,1,1};
 		int[] arr2 = {1,1,1,1,1,0,1};
-		int len1 = arr1.length;
-		int len2 = arr2.length;
-		System.out.println("MaxLen: "+ longestSpan(arr1, arr2, len1, len2));
+		int len = arr1.length;
+		System.out.println("MaxLen: "+ longestSpan(arr1, arr2, len));
 	}
 	
-	public static int longestSpan(int[] arr1, int[] arr2, int len1, int len2) {
+	public static int longestSpan(int[] arr1, int[] arr2, int len) {
 		int maxLen =0;
         //Points to take:
 		//int sum1 =0, sum2=0;  Writing this statement here will change answer.
-		for(int i=0; i<len1; i++ ) {
+		for(int i=0; i<len; i++ ) {
 			
 			//Optimization1: adding this check ensures no check when maxLen is greater then
 			//remaining array
-			if(maxLen > len1-i)
+			if(maxLen > len-i)
 				break;
 			int sum1 =0, sum2=0;
-			for(int j=i; j<len2; j++) {
+			for(int j=i; j<len; j++) {
 				sum1 += arr1[j];
 				sum2 += arr2[j];
 				
 				if(sum1 == sum2) {
-					int len = j-i +1;
-					if(len >maxLen) {
-						maxLen= len;
+					int length = j-i +1;
+					if(length >maxLen) {
+						maxLen= length;
 					}
 				}
 			}
