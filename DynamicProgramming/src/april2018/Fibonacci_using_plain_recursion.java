404a731def87fa8222204843a4347618afd943ef
abhishek <abhiwanttochat@gmail.com>
2018-04-10 21:06:23 +0530

    Comment added for Fibonacci with plIN recursion

--- DynamicProgramming/src/april2018/Fibonacci_using_plain_recursion.java
+++ DynamicProgramming/src/april2018/Fibonacci_using_plain_recursion.java
@@ -1,5 +1,8 @@
 package april2018;
 
+//T(n) = T(n-1) + T(n-2) + O(1) -> to sum both results
+//where, T(n<= 1) = O(1)
+//Intuitively , T(n)= O(2^n)
 public class Fibonacci_using_plain_recursion {
 	public static void main(String[] args) {
 		System.out.println(fib(5));
