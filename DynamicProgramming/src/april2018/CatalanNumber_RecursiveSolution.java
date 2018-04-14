88f6e4dc4820c870c38b3859e0187531d3eb8c6d
abhishek <abhiwanttochat@gmail.com>
2018-04-14 18:38:28 +0530

    Catalan Number - Recursive solution

---
+++ DynamicProgramming/src/april2018/CatalanNumber_RecursiveSolution.java
@@ -0,0 +1,21 @@
+package april2018;
+
+public class CatalanNumber_RecursiveSolution {
+	public static void main(String[] args) {
+		System.out.println(catalan(4));
+	}
+	
+	public static long catalan(int n) {
+	   long result= 0;
+	   
+	   //Base case
+	   if(n<=1)
+		   return 1;
+	   
+	   //catalan(i) is sum of catalan(i)* catalan(n-i-1)
+	   for(int i=0; i<n ; i++) {
+		   result += catalan(i)* catalan(n-i -1);
+	   }
+		return result;   
+	}
+}
