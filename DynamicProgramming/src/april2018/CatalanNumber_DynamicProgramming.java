b4476af2e59c3099b210c3840c7e94bee81825c1
abhishek <abhiwanttochat@gmail.com>
2018-04-15 01:54:34 +0530

    Catalan Number - Dynamic Programming technique

---
+++ DynamicProgramming/src/april2018/CatalanNumber_DynamicProgramming.java
@@ -0,0 +1,22 @@
+package april2018;
+
+public class CatalanNumber_DynamicProgramming {
+	public static void main(String[] args) {
+		System.out.println(catalan(4));
+	}
+	
+	public static long catalan(int n) {
+		long[] catalan = new long[n+1];
+		
+		catalan[0]= catalan[1]=1;
+		
+		for(int i=2; i<= n;i++) {
+			//catalan[i] =0;
+			for(int j=0; j<i; j++) {
+				catalan[i] += catalan[j] * catalan[i-j-1];
+			}
+		}
+		
+		return catalan[n];
+	}
+}
