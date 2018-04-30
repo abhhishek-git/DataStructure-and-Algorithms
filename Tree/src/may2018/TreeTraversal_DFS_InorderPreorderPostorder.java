fd2029c6da6eddb126cf2807d818eb2690f12e43
abhishek <abhiwanttochat@gmail.com>
2018-04-30 09:35:35 +0530

    Tree traversal DFS - Inorder Preorder Postorder

--- Tree/src/may2018/TreeTraversal_DFS_InorderPreorderPostorder.java
+++ Tree/src/may2018/TreeTraversal_DFS_InorderPreorderPostorder.java
@@ -1,5 +1,65 @@
 package may2018;
 
+//O(n)
+//T(n)  = T(k) + T(n-k-1) + c
+class Node{
+	Node left, right;
+	int key;
+	Node(int item){
+		key = item;
+		left = null;
+		right = null;
+	}
+}
 public class TreeTraversal_DFS_InorderPreorderPostorder {
-
+  Node root;
+  
+  public static void main(String[] args) {
+	TreeTraversal_DFS_InorderPreorderPostorder tree = new TreeTraversal_DFS_InorderPreorderPostorder();
+	tree.root = new Node(1);
+	tree.root.left = new Node(2);
+	tree.root.right= new Node(3);
+	tree.root.left.left = new Node(4);
+	tree.root.left.right = new Node(5);
+	
+	System.out.println("Preorder traversal of the binary tree is: ");
+	tree.preorder();
+	System.out.println("\nPostorder traversal of the binary tree is: ");
+	tree.postorder();
+	System.out.println("\nInorder traversal of the binary tree is: ");
+	tree.inorder();
+}
+  void preorder() {
+	  preorder(root);
+  }
+  void postorder() {
+	  postorder(root);
+  }
+  void inorder() {
+	  inorder(root);
+  }
+  
+  public void preorder(Node node) {
+	  if(node == null)
+		  return;
+	  System.out.print(node.key+ " ");
+	  preorder(node.left);
+	  preorder(node.right);
+  }
+  
+  public void postorder(Node node) {
+	  if(node == null)
+		  return;
+	  postorder(node.left);
+	  postorder(node.right);
+	  System.out.print(node.key+ " ");
+  }
+  
+  public void inorder(Node node) {
+	  if(node == null)
+		  return;
+	  inorder(node.left);
+	  System.out.print(node.key+ " ");
+	  inorder(node.right);
+  }
 }
