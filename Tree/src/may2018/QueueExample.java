7d561b4609edb0b2a08880468571f1bdcaa27344
abhishek <abhiwanttochat@gmail.com>
2018-05-03 07:18:12 +0530

    Queue Example

---
+++ Tree/src/may2018/QueueExample.java
@@ -0,0 +1,17 @@
+package may2018;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>();
+		for(int i=0; i<5 ;i++)
+			q.add(i);
+		
+		System.out.println(q);
+		System.out.println(q.remove());
+		System.out.println(q.peek());
+		System.out.println(q.size());
+	}
+}
