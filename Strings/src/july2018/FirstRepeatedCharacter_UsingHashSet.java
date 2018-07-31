454e5336eaf8810734a973be64216632bb3753ba
abhishek singh <abhiwanttochat@gmail.com>
2018-08-01 00:39:09 +0530

    Comment update

--- Strings/src/july2018/FirstRepeatedCharacter_UsingHashSet.java
+++ Strings/src/july2018/FirstRepeatedCharacter_UsingHashSet.java
@@ -1,8 +1,11 @@
 package july2018;
 
+//Note that this method using HashSet will give different than HashMap, as other method first 
+//creates the Map and then check one by one. Hence for "geeksforgeeks" , output is g using HashMap
+//while output is e using HashSet method
 import java.util.HashSet;
 
-public class FirstRepeatedCharacter_UsingHashing {
+public class FirstRepeatedCharacter_UsingHashSet {
 	public static void main(String[] args) {
 		String str ="geeksforgeeks";
 		char[] arr = str.toCharArray();
